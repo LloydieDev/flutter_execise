535a1f564aaf15757ec89d20ade7b948ffe6ca69
LloydieDev <johnlloyd.espada@gmail.com>
2024-01-24 17:18:10 +0800

    added QuizApp start screen

--- lib/QuizApp/quiz_app_onboard.dart
+++ lib/QuizApp/quiz_app_onboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_device/QuizApp/quiz_app_home.dart';
 
 class QuizAppOnBoard extends StatelessWidget {
   const QuizAppOnBoard({super.key});
@@ -6,11 +7,53 @@ class QuizAppOnBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset('lib/QuizApp/image/quiz-logo.png', height: 20, width: 20,),
-          ElevatedButton(onPressed: (){}, child: Text('Quiz'))
-        ],
+      backgroundColor: Colors.pinkAccent,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Center(
+              child: Image.asset('lib/QuizApp/image/quiz_logo.png'),
+            ),
+            const SizedBox(height: 100),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const QuizAppHome(),
+                  ),
+                );
+              },
+              style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  fixedSize: const Size(200, 50),
+                  side: const BorderSide(color: Colors.white),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.keyboard_double_arrow_right,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'Quiz App',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
